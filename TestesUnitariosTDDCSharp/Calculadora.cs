d211e06af03a61c05bbe637e697b571c0ec747af
anaBeatrizWalker <anabiawalker@gmail.com>
2024-09-27 15:07:51 -0300

    Parte 1: Adicionando os testes

---
+++ TestesUnitariosTDDCSharp/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestesUnitariosTDDCSharp
+{
+    public class Calculadora
+    {
+        public int somar(int a, int b)
+        {
+            return 0;
+        }
+
+        public int subtrair(int a, int b)
+        {
+            return 0;
+        }
+
+        public int multiplicar(int a, int b)
+        {
+            return 0;
+        }
+
+        public int dividir(int a, int b)
+        {
+            return 0;
+        }
+
+        public List<string> historico()
+        {
+            return new List<string>();
+        }
+    }
+}
--- TestesUnitariosTDDCSharpTests/UnitTest1.cs
+++ TestesUnitariosTDDCSharpTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using TestesUnitariosTDDCSharp;
 using Xunit;
 
 namespace TestesUnitariosTDDCSharpTests
@@ -6,8 +7,86 @@ namespace TestesUnitariosTDDCSharpTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TesteSomarUmConjunto()
         {
+            Calculadora calculadora = new Calculadora();
+
+            int resultado = calculadora.somar(1, 2);
+
+            Assert.Equal(3, resultado);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 3, 5)]
+        public void TesteSomarVariosConjuntos(int a, int b, int resultado)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            int resultadoDaCalculadora = calculadora.somar(a, b);
+
+            Assert.Equal(resultado, resultadoDaCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(6, 5, 1)]
+        public void TesteSubtrair(int a, int b, int resultado)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            int resultadoDaCalculadora = calculadora.subtrair(a, b);
+
+            Assert.Equal(resultado, resultadoDaCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TesteMultiplicar(int a, int b, int resultado)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            int resultadoDaCalculadora = calculadora.multiplicar(a, b);
+
+            Assert.Equal(resultado, resultadoDaCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(5, 5, 1)]
+        public void TesteDividir(int a, int b, int resultado)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            int resultadoDaCalculadora = calculadora.dividir(a, b);
+
+            Assert.Equal(resultado, resultadoDaCalculadora);
+        }
+
+        [Fact]
+        public void TesteDivisaoPorZero()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            Assert.Throws<DivideByZeroException>(
+                   () => calculadora.dividir(3,0)
+            );
+        }
+
+        [Fact]
+        public void TesteHistoricoNaoVazio()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            calculadora.somar(1, 2);
+            calculadora.subtrair(2, 1);
+            calculadora.multiplicar(1, 2);
+
+            var lista = calculadora.historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(4, lista.Count);
         }
     }
 }
